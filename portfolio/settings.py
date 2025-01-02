6fadc7fbde3958f941f7e5fe686d5b28a4dd602f
jayasaisrikar <bjayasaisrikar2004@gmail.com>
2025-01-03 02:12:06 +0530

    added vercel_app.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
+    }
 }
 
 
---
+++ vercel_app.py
@@ -0,0 +1,2 @@
+from portfolio.wsgi import application
+app = application 
