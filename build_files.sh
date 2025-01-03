ae411baa08f53d444c7dd787e6b54f8c13ed41b3
jayasaisrikar <bjayasaisrikar2004@gmail.com>
2025-01-03 23:43:57 +0530

    made changes for deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
-pip install -r requirements.txt
+#!/bin/bash
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
--- wsgi_handler.py
+++ wsgi_handler.py
@@ -1,11 +1,18 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from django.core.management import execute_from_command_line
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-# Run migrations
-execute_from_command_line(['manage.py', 'migrate'])
+# Initialize Django
+django.setup()
+
+try:
+    # Run migrations
+    execute_from_command_line(['manage.py', 'migrate'])
+except Exception as e:
+    print(f"Migration error: {str(e)}")
 
 # Get the WSGI application
 application = get_wsgi_application()
