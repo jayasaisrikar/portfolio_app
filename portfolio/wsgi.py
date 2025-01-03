cdbdeac20de12bd3e313b968182d91d3158c461a
jayasaisrikar <bjayasaisrikar2004@gmail.com>
2025-01-04 00:10:47 +0530

    updated vercel.json(will give error)

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,12 +8,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from django.conf import settings
+from wsgi_handler import application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(settings.STATIC_ROOT, prefix='static/')
 app = application
--- wsgi_handler.py
+++ wsgi_handler.py
@@ -1,16 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.core.management import execute_from_command_line
-import django
-from django.contrib.auth.models import User
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 application = get_wsgi_application()
-try:
-    django.setup()
-    execute_from_command_line(['manage.py', 'migrate'])
-    
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', os.environ.get('ADMIN_PASSWORD', 'admin'))
-except Exception as e:
-    print(f"Setup error: {str(e)}")
+
+# Configure WhiteNoise
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.STATIC_ROOT, prefix='static/')
