aa4f5195a93332a73d8ddad73132b36e3e151fff
jayasaisrikar <bjayasaisrikar2004@gmail.com>
2025-01-03 23:46:48 +0530

    to add styles

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput --clear
