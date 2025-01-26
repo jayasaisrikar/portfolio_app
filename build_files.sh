93883b93a0caea1345c889eeb3864a9a266debce
jayasaisrikar <bjayasaisrikar2004@gmail.com>
2025-01-26 19:05:29 +0530

    changes to include staticfiles

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
 # Build script for Vercel
 pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput
 mkdir -p media
 chmod -R 755 media
